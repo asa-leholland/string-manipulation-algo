b52667d76c2830be9fb2334bec5516518e9bc53e
asa-leholland <asaleholland@gmail.com>
2023-02-19 21:23:24 -0800

    REF: removed print statemnts

--- main.py
+++ main.py
@@ -14,29 +14,24 @@ def transform_to_combination_array(input: str) -> list[str]:
     lists_between_ors.append(temp)
 
     for choice_list in lists_between_ors:
-        print('final_results', final_results)
-        print('choice_list', choice_list)
 
         if len(final_results) == 0:
             final_results = choice_list
             continue
         if len(choice_list) == 1:
             for result, i in enumerate(final_results):
-                print('result', result)
-                final_results[i] = result + " " + choice_list[0]
+                final_results[i] = f"{result} {choice_list[0]}"
             for result in final_results:
                 result = final_results.append(choice_list[0])
             continue
 
-        choice_count = len(choice_list)
         temp = []
         for choice in choice_list:
-            for result in final_results:
-                temp.append(f'{result} {choice}')
+            temp.extend(f'{result} {choice}' for result in final_results)
         final_results = temp
 
-
     return final_results
+
 
 
 if __name__ == "__main__":
